4af34d054e635c2bb89d46314ac4a9aded062343
Creplav <creplav@gmail.com>
2017-11-22 20:28:24 -0500

    Fixed a bunch of issues and made minor changes

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -17,12 +18,16 @@ import javafx.stage.Stage;
  */
 public class Login {
 
+
+     public static boolean isValid(String pw) {
+        return pw.matches("[a-zA-Z0-9]{8,}");
+}
     public void show(Stage stage){
         GridPane pane = new GridPane();
         stage.setTitle("Login");
         stage.setScene(new Scene(pane, 500, 300));
         TextField nameField = new TextField();
-        TextField rocketNumberField = new TextField();
+        PasswordField rocketNumberField = new PasswordField();
         pane.add(new Label("Login"), 1, 0);
         pane.add(new Label("Please enter your name: "), 0, 1);
         pane.add(nameField, 1, 1);
@@ -35,9 +40,20 @@ public class Login {
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                boolean valid = true;
+                for (int i = 1; i < rocketNumberField.getText().length() -1; i++)
+                {
+                     char l = rocketNumberField.getText().charAt(i);
+                     if (rocketNumberField.getText().length() < 9 && !Character.isLetter(l) || !Character.isDigit(l))
+                     {
+                         pane.add(new Label("The password is not in the required format" +
+                                 "\nFormat: R00000000"), 0, 5);
+                        return;
+                     }
+                }
                 if(nameField.getText().equals("") || rocketNumberField.getText().equals("")){
                     //TODO Keep this from constantly creating
-                    pane.add(new Label("At least one required text field is empty"), 0,5);
+                    pane.add(new Label("At least one required text field is empty"), 0,6);
                     return;
                 }
                 //((Node)(event.getSource())).getScene().getWindow().hide();
